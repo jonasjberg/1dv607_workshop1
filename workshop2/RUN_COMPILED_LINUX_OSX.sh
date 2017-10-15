300ef39e8d43512a2155a8a65667d9fbd4ffd221
Jonas Sjöberg <jomeganas@gmail.com>
2017-10-15 19:49:01 +0200

    Add launcher scripts for compiled versions.

---
+++ workshop2/RUN_COMPILED_LINUX_OSX.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# JOLLY PIRATE launcher script for MacOS/Linux
+# ============================================
+# Copyright(c) 2017 Jonas Sjöberg
+# https://github.com/jonasjberg
+# http://www.jonasjberg.com
+# University mail: js224eh[a]student.lnu.se
+
+set -o nounset -o pipefail
+
+./build/jollypirate_linux
--- workshop2/RUN_COMPILED_WINDOWS.bat
+++ workshop2/RUN_COMPILED_WINDOWS.bat
@@ -9,6 +9,6 @@ REM University mail: js224eh[a]student.lnu.se
 
 SET self_path=%~dp0
 pushd %self_path%
-python3 jollypirate
+build/jollypirate.exe
 popd
 
